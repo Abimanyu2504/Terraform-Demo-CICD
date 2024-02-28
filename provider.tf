b3eaad346a688157ca3d9bf3533bcea333e2e94b
Abimanyu2504 <abimanyu2504@gmail.com>
2024-02-28 18:25:03 +0530

    added access and secret key

--- provider.tf
+++ provider.tf
@@ -10,6 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  #access_key = var.AWS_ACCESS_KEY_ID
-  #secret_key = var.AWS_SECRET_KEY_ID
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_KEY_ID
 }
